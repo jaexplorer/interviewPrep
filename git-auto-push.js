a3ebe1773781a7fe37f9d66226a7bc8bbbbeae2e
Jaexplorer <andrewsabato8@gmail.com>
2025-06-06 23:36:21 +1000

    Saving changes

--- git-auto-push.js
+++ git-auto-push.js
@@ -1,9 +1,12 @@
 const { exec } = require("child_process");
 
-exec('git add . && git commit -m "save" && git push', (err, stdout, stderr) => {
-  if (err) {
-    console.error("❌ Error:", stderr.trim());
-    return;
+exec(
+  'git add . && git commit -m "Saving changes" && git push',
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error("❌ Error:", stderr.trim());
+      return;
+    }
+    console.log("✅ Git push complete:\n", stdout.trim());
   }
-  console.log("✅ Git push complete:\n", stdout.trim());
-});
+);
